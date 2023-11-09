0da65573a4709948d109da4c48487e21267aa083
unaisek <mohammedunaiseknkt@gmail.com>
2023-11-09 12:09:08 +0530

    signup page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,57 @@ import { FormBuilder, Validator, Validators } from "@angular/forms";
 export class AppComponent {
   title = 'angular-material';
   submitted = false;
+  signUpSubmitted =false
   hide = true;
+  signUphide = true;
+
+  jobes = [
+    {id:1,name:"student"},
+    {id:2,name:"employee"},
+    {id:3,name:"self-employee"},
+    {id:4,name:"business"}
+  ]
   constructor( private fb:FormBuilder){}
 
   loginForm = this.fb.group({
     email:['',[Validators.required,Validators.email]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required]],
+    
   });
+
+  signUpForm = this.fb.group({
+    firstName:['',[Validators.required]],
+    lastName: ['', [Validators.required]],
+    gender: ['', [Validators.required]],
+    dob:['',[Validators.required]],
+    job:['',[Validators.required]],
+    password:['',[Validators.required]],
+    confirmPassword:['',[Validators.required]]
+  }, {
+    validator:()=>{
+      console.log(this.signUpForm);
+      if (this.signUpForm?.controls?.['password'].value != this.signUpForm?.controls?.['confirmPassword'].value ) {
+        this.signUpForm?.controls?.['confirmPassword'].setErrors({passwordMismatch:true})
+      }
+    }
+  }
+  
+  )
 
   get allControls(){
     return this.loginForm.controls
   }
 
+  get allControlsdSignUp(){
+    return this.signUpForm.controls
+  }
+
+
   onSubmit(values:any){
-    this.submitted = true;
-    console.log(values);
-    
+    this.submitted = true;    
+  }
+
+  onSubmitSignUp(values:any){
+    this.signUpSubmitted = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -26,10 +32,14 @@ import { MatIconModule } from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatIconModule  
+    MatIconModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatSelectModule  
     
   ],
-  providers: [],
+  providers: [{ provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
